5f2e93c4874e7878570377370d911b206bcda9a3
unknown <zaidrabbani65@student.ub.ac.id>
2026-01-14 18:11:43 +0700

    fix: fixing to deploy in vercel

--- api/index.js
+++ api/index.js
@@ -1,13 +1,31 @@
-const createServer = require('../src/Infrastructures/http/createServer');
-const container = require('../src/Infrastructures/container');
+const createServer = require("../src/Infrastructures/http/createServer");
+const container = require("../src/Infrastructures/container");
 
 let server;
 
-module.exports = async (req, res) => {
-    if (!server) {
-        server = await createServer(container);
-        await server.initialize();
-    }
+const handler = async (req, res) => {
+  if (!server) {
+    server = await createServer(container);
+    await server.initialize();
+  }
 
-    server.listener(req, res);
+  // Hapi.js inject method untuk serverless
+  const response = await server.inject({
+    method: req.method,
+    url: req.url,
+    headers: req.headers,
+    payload: req.body,
+    remoteAddress: req.connection?.remoteAddress,
+  });
+
+  // Set headers dari response Hapi
+  res.statusCode = response.statusCode;
+  Object.keys(response.headers).forEach((key) => {
+    res.setHeader(key, response.headers[key]);
+  });
+
+  // Send response
+  res.end(response.result);
 };
+
+module.exports = handler;
